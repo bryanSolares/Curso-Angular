afdffd572b441870d51a92a04466d1acdf8a992f
Bryan Solares i7 <solares.josue@outlook.com>
2020-06-26 21:24:56 -0600

    Se avanza hasta sección 4.52

--- Angular/01-hola-mundo/src/app/app.module.ts
+++ Angular/01-hola-mundo/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, BodyComponent, FooterComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
