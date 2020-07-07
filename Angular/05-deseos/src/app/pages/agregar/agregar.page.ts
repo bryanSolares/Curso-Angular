169a3896e2c56ed5e1d8a9be4e0adc3fc65477a3
Bryan linux-mint <solares.josue@outlook.com>
2020-07-06 21:24:57 -0600

    Se avanza hasta sección 7.127

---
+++ Angular/05-deseos/src/app/pages/agregar/agregar.page.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { DeseosService } from "../../services/deseos.service";
+import { Lista } from "../../models/lista.model";
+import { ListaItem } from "../../models/lista-item.model";
+
+@Component({
+  selector: "app-agregar",
+  templateUrl: "./agregar.page.html",
+})
+export class AgregarPage implements OnInit {
+  lista: Lista;
+  nombreItem: string = "";
+
+  constructor(
+    private deseosService: DeseosService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    const listaId = activatedRoute.snapshot.paramMap.get("listaId");
+    this.lista = this.deseosService.obtenerLista(listaId);
+  }
+
+  ngOnInit() {}
+
+  agregarItem() {
+    if (this.nombreItem.length === 0) {
+      return;
+    }
+
+    const nuevoItem = new ListaItem(this.nombreItem);
+    this.lista.items.push(nuevoItem);
+    this.nombreItem = "";
+    this.deseosService.guardarStorage();
+  }
+
+  cambioCheck(item: ListaItem) {
+    const pendientes = this.lista.items.filter((item) => !item.completado)
+      .length;
+    if (pendientes === 0) {
+      this.lista.terminadaEn = new Date();
+      this.lista.terminada = true;
+    }else{
+      this.lista.terminadaEn = null;
+      this.lista.terminada = false;
+    }
+    this.deseosService.guardarStorage();
+    console.log(this.deseosService.listas[0]);
+  }
+}
---
+++ Angular/05-deseos/src/app/services/deseos.service.ts
@@ -0,0 +1,37 @@
+import { Lista } from "./../models/lista.model";
+import { Injectable } from "@angular/core";
+import { Lista } from '../models/lista.model';
+
+@Injectable({
+  providedIn: "root",
+})
+export class DeseosService {
+  listas: Lista[] = [];
+  constructor() {
+    this.cargarStorage();
+  }
+
+  crearLista(titulo: string) {
+    const nuevaLista = new Lista(titulo);
+    this.listas.push(nuevaLista);
+    this.guardarStorage();
+    return nuevaLista.id;
+  }
+
+  obtenerLista(idLista: string | number): Lista {
+    idLista = Number(idLista);
+    return this.listas.find((listaData) => listaData.id === idLista);
+  }
+
+  guardarStorage() {
+    localStorage.setItem("data", JSON.stringify(this.listas));
+  }
+
+  cargarStorage() {
+    if (localStorage.getItem("data")) {
+      this.listas = JSON.parse(localStorage.getItem("data"));
+    } else {
+      this.listas = [];
+    }
+  }
+}
